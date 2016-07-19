63d47db82c8a407bf07047d4e4e0101cb6e22902
Shadi <TheSisb@users.noreply.github.com>
2016-07-18 17:16:25 -0700

    Ability to specify a file size limit

--- index.js
+++ index.js
@@ -6,22 +6,31 @@ var loaderUtils = require('loader-utils');
 
 module.exports = function(content) {
 	this.cacheable && this.cacheable();
-
+	
 	var query = loaderUtils.parseQuery(this.query);
-
-	content = content.toString('utf8');
-	content = content.replace(/"/g, "'");
-	content = content.replace(/\s+/g, " ");
-	content = content.replace(/[{}\|\\\^~\[\]`"<>#%]/g, function(match) {
-		return '%'+match[0].charCodeAt(0).toString(16).toUpperCase();
-	});
-
-	var data = 'data:image/svg+xml;charset=utf8,' + content.trim();
-	if (!query.noquotes) {
-		data = '"'+data+'"';
+	var limit = (this.options && this.options.url && this.options.url.dataUrlLimit) || 0;
+	if(query.limit) {
+		limit = parseInt(query.limit, 10);
 	}
-
-	return 'module.exports = ' + JSON.stringify(data);
+	
+	if(limit <= 0 || content.length < limit) {
+		content = content.toString('utf8');
+		content = content.replace(/"/g, "'");
+		content = content.replace(/\s+/g, " ");
+		content = content.replace(/[{}\|\\\^~\[\]`"<>#%]/g, function(match) {
+			return '%'+match[0].charCodeAt(0).toString(16).toUpperCase();
+		});
+	
+		var data = 'data:image/svg+xml;charset=utf8,' + content.trim();
+		if (!query.noquotes) {
+			data = '"'+data+'"';
+		}
+		
+		return 'module.exports = ' + JSON.stringify(data);
+	} else {
+		var fileLoader = require("file-loader");
+		return fileLoader.call(this, content);
+	}
 };
 
 module.exports.raw = true;
