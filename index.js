4bfa8519d18f9ee4a58cd2bb9a3bd54b5a27baa7
bhovhannes <bhovhannes@gmail.com>
2017-02-27 15:05:57 +0400

    fix: updated to latest loader-utils

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var loaderUtils = require('loader-utils');
 module.exports = function(content) {
 	this.cacheable && this.cacheable();
 
-	var query = loaderUtils.parseQuery(this.query);
+	var query = loaderUtils.getOptions(this);
 	
 	var limit = query.limit ? parseInt(query.limit, 10) : 0;
 	
