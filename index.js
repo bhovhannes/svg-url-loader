74b2c9c4eccad1f96faf928c81c1017663cfdb21
Kayne Barclay <kbarclay@atlassian.com>
2017-10-18 17:58:10 -0700

    fixes #141: Allows for a new option 'base64' to enable base64 encoding, while still taking advantage of the 4096 byte limit checking for IE 11

--- index.js
+++ index.js
@@ -20,16 +20,25 @@ module.exports = function(content) {
 			newContent = newContent.replace(/^\s*<\?xml [^>]*>\s*/i, "");
 		}
 
-		newContent = newContent.replace(/"/g, "'");
-		newContent = newContent.replace(/\s+/g, " ");
-		newContent = newContent.replace(/[{}\|\\\^~\[\]`"<>#%]/g, function(match) {
-			return '%'+match[0].charCodeAt(0).toString(16).toUpperCase();
-		});
+		var data;
+		if (query.base64) {
+			if (typeof newContent === "string") {
+				newContent = new Buffer(newContent);
+			}
+			data = "data:image/svg+xml;base64," + newContent.toString("base64");
+		} else {
+			newContent = newContent.replace(/"/g, "'");
+			newContent = newContent.replace(/\s+/g, " ");
+			newContent = newContent.replace(/[{}\|\\\^~\[\]`"<>#%]/g, function(match) {
+				return '%'+match[0].charCodeAt(0).toString(16).toUpperCase();
+			});
 
-		var data = 'data:image/svg+xml,' + newContent.trim();
+			data = 'data:image/svg+xml,' + newContent.trim();
+
+		}
 
 		if (!(query.iesafe && hasStyleElement && data.length > 4096)) {
-			if (!query.noquotes) {
+			if (!query.base64 && !query.noquotes) {
 				data = '"'+data+'"';
 			}
 
