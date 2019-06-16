758b6478fd0b8ed91c864b47a72227d733d89bec
bhovhannes <bhovhannes@gmail.com>
2019-06-17 00:32:49 +0400

    perf: increase loader performance by avoiding re-parsing regexps

--- index.js
+++ index.js
@@ -4,6 +4,13 @@
  */
 var loaderUtils = require('loader-utils');
 
+var REGEX_STYLE = /<style[\s\S]*?>[\s\S]*?<\/style>/i
+var REGEX_DECLARATION = /^\s*<\?xml [^>]*>\s*/i
+
+var REGEX_DOUBLE_QUOTE = /"/g
+var REGEX_MULTIPLE_SPACES = /\s+/g
+var REGEX_UNSAFE_CHARS = /[{}\|\\\^~\[\]`"<>#%]/g
+
 module.exports = function(content) {
 	this.cacheable && this.cacheable();
 
@@ -15,10 +22,10 @@ module.exports = function(content) {
 	if (limit <= 0 || content.length < limit) {
 		var newContent = content.toString('utf8');
 
-		var hasStyleElement = /<style[\s\S]*?>[\s\S]*?<\/style>/i.test(newContent)
+		var hasStyleElement = REGEX_STYLE.test(newContent)
 
 		if (query.stripdeclarations) {
-			newContent = newContent.replace(/^\s*<\?xml [^>]*>\s*/i, "");
+			newContent = newContent.replace(REGEX_DECLARATION, "");
 		}
 
 		var data;
@@ -28,9 +35,9 @@ module.exports = function(content) {
 			}
 			data = "data:image/svg+xml;base64," + newContent.toString("base64");
 		} else {
-			newContent = newContent.replace(/"/g, "'");
-			newContent = newContent.replace(/\s+/g, " ");
-			newContent = newContent.replace(/[{}\|\\\^~\[\]`"<>#%]/g, function(match) {
+			newContent = newContent.replace(REGEX_DOUBLE_QUOTE, "'");
+			newContent = newContent.replace(REGEX_MULTIPLE_SPACES, " ");
+			newContent = newContent.replace(REGEX_UNSAFE_CHARS, function(match) {
 				return '%'+match[0].charCodeAt(0).toString(16).toUpperCase();
 			});
 
