9b58a0f05ef329ae5e55f90937c6432a23ea1fc1
Hovhannes Babayan <bhovhannes@gmail.com>
2022-09-04 19:46:03 +0400

    feat: upgraded to webpack 5
    BREAKING CHANGE: loader won't work in v4 anymore

--- src/loader.js
+++ src/loader.js
@@ -1,71 +1,66 @@
-const { getOptions, parseQuery } = require("loader-utils");
-
-const REGEX_STYLE = /<style[\s\S]*?>[\s\S]*?<\/style>/i;
-const REGEX_XML_DECLARATION = /^\s*<\?xml [^>]*>\s*/i;
-const REGEX_MULTIPLE_SPACES = /\s+/g;
-const REGEX_DOUBLE_QUOTE = /"/g;
-const REGEX_UNSAFE_CHARS = /[{}\|\\\^~\[\]`"<>#%]/g;
+const REGEX_STYLE = /<style[\s\S]*?>[\s\S]*?<\/style>/i
+const REGEX_XML_DECLARATION = /^\s*<\?xml [^>]*>\s*/i
+const REGEX_MULTIPLE_SPACES = /\s+/g
+const REGEX_DOUBLE_QUOTE = /"/g
+const REGEX_UNSAFE_CHARS = /[{}\|\\\^~\[\]`"<>#%]/g
 
 module.exports = function (content) {
-  this.cacheable && this.cacheable();
+  this.cacheable && this.cacheable()
 
   let query = {
-    encoding: "none",
+    encoding: 'none',
     stripdeclarations: true,
-    ...getOptions(this),
-    ...(this.resourceQuery ? parseQuery(this.resourceQuery) : undefined),
-  };
+    ...this.getOptions()
+  }
 
-  let limit = query.limit ? parseInt(query.limit, 10) : 0;
-
-  if (limit <= 0 || content.length < limit) {
-    const originalSvgSource = content.toString("utf8");
-
-    const transformedSvgSource = applyGeneralTransforms(
-      originalSvgSource,
-      query.stripdeclarations
-    );
-
-    let data;
-    if (query.encoding === "base64") {
-      data =
-        "data:image/svg+xml;base64," +
-        Buffer.from(transformedSvgSource).toString("base64");
-    } else {
-      data = "data:image/svg+xml," + uriEncode(transformedSvgSource);
-    }
-
-    if (
-      !query.iesafe ||
-      data.length <= 4096 ||
-      !REGEX_STYLE.test(originalSvgSource)
-    ) {
-      return "module.exports = " + JSON.stringify(data);
+  if (this.resourceQuery) {
+    let queryParams = new URLSearchParams(this.resourceQuery)
+    for (const [k, v] of queryParams) {
+      query[k] = v
     }
   }
 
-  const fileLoader = require("file-loader");
-  const childContext = Object.create(this);
-  childContext.query = Object.assign({}, query);
-  return fileLoader.call(childContext, content);
-};
+  let limit = query.limit ? parseInt(query.limit, 10) : 0
 
-module.exports.raw = true;
+  if (limit <= 0 || content.length < limit) {
+    const originalSvgSource = content.toString('utf8')
+
+    const transformedSvgSource = applyGeneralTransforms(originalSvgSource, query.stripdeclarations)
+
+    let data
+    if (query.encoding === 'base64') {
+      data = 'data:image/svg+xml;base64,' + Buffer.from(transformedSvgSource).toString('base64')
+    } else {
+      data = 'data:image/svg+xml,' + uriEncode(transformedSvgSource)
+    }
+
+    if (!query.iesafe || data.length <= 4096 || !REGEX_STYLE.test(originalSvgSource)) {
+      return 'module.exports = ' + JSON.stringify(data)
+    }
+  }
+
+  const fileLoader = require('file-loader')
+  const childContext = Object.create(this)
+  childContext.query = Object.assign({}, query)
+  return fileLoader.call(childContext, content)
+}
+
+module.exports.raw = true
 
 function uriEncodeMatch(match) {
-  return "%" + match[0].charCodeAt(0).toString(16).toLowerCase();
+  return '%' + match[0].charCodeAt(0).toString(16).toLowerCase()
 }
 
 function uriEncode(svgSource) {
-  svgSource = svgSource.replace(REGEX_DOUBLE_QUOTE, "'");
-  svgSource = svgSource.replace(REGEX_UNSAFE_CHARS, uriEncodeMatch);
-  return svgSource.trim();
+  svgSource = svgSource.replace(REGEX_DOUBLE_QUOTE, "'")
+  svgSource = svgSource.replace(REGEX_UNSAFE_CHARS, uriEncodeMatch)
+  return svgSource.trim()
 }
 
 function applyGeneralTransforms(svgSource, stripXmlDeclaration) {
   if (stripXmlDeclaration) {
-    svgSource = svgSource.replace(REGEX_XML_DECLARATION, "");
+    svgSource = svgSource.replace(REGEX_XML_DECLARATION, '')
   }
-  svgSource = svgSource.replace(REGEX_MULTIPLE_SPACES, " ");
-  return svgSource;
+  svgSource = svgSource.replace(REGEX_MULTIPLE_SPACES, ' ')
+  return svgSource
 }
--- test/loader.spec.js
+++ test/loader.spec.js
@@ -1,27 +1,25 @@
-const { describe, it, afterEach, expect } = require("@jest/globals");
-const fsPromises = require("fs").promises;
-const path = require("path");
-const webpack = require("webpack");
+const { describe, it, expect } = require('@jest/globals')
+const path = require('path')
+const webpack = require('webpack')
 
-const svgUrlLoader = path.resolve(__dirname, "../index.js");
-const context = path.resolve(__dirname, "../");
-const outputDir = path.resolve(__dirname, "./output");
-const bundleFileName = "bundle.js";
+const svgUrlLoader = path.resolve(__dirname, '../index.js')
+const context = path.resolve(__dirname, '../')
+const outputDir = path.resolve(__dirname, './output')
 
-function getBundleFile() {
-  return path.join(outputDir, bundleFileName);
+function getBundleFile(assetName) {
+  return path.join(outputDir, assetName)
 }
 
 function getBaseWebpackConfig() {
   return {
     context,
-    mode: "development",
+    mode: 'development',
     devtool: false,
     output: {
       path: outputDir,
-      filename: bundleFileName,
-      publicPath: "",
-      libraryTarget: "commonjs2",
+      filename: 'bundle.[fullhash].js',
+      publicPath: '',
+      libraryTarget: 'commonjs2'
     },
     module: {
       rules: [
@@ -31,237 +29,232 @@ function getBaseWebpackConfig() {
           use: [
             {
               loader: svgUrlLoader,
-              options: {},
-            },
+              options: {}
+            }
           ],
-        },
-      ],
-    },
-  };
+          type: 'javascript/auto'
+        }
+      ]
+    }
+  }
 }
 
 async function runWebpack(config) {
   return new Promise((resolve, reject) => {
-    webpack(config, function (err, data) {
+    webpack(config, function (err, stats) {
       if (err) {
-        reject(err);
+        reject(err)
       } else {
-        resolve(data);
+        resolve(stats.toJson('minimal').assetsByChunkName.main[0])
       }
-    });
-  });
+    })
+  })
 }
 
-async function evaluateGeneratedBundle() {
-  const buffer = await fsPromises.readFile(getBundleFile());
-  const content = buffer.toString();
-  return eval(content);
+async function evaluateGeneratedBundle(assetName) {
+  // require is now mandatory with webpack >5.22.0
+  return eval(`require("${getBundleFile(assetName)}")`)
 }
 
-describe("svg-url-loader", function () {
-  // Clean generated cache files before each test so that we can call each test with an empty state.
-  afterEach(() => fsPromises.unlink(getBundleFile()));
-
+describe('svg-url-loader', function () {
   describe('"stripdeclarations" option', function () {
-    it("if turned off - should do nothing to an SVG that has an XML declaration", async () => {
+    it('if turned off - should do nothing to an SVG that has an XML declaration', async () => {
       const config = {
         ...getBaseWebpackConfig(),
-        entry: "./test/input/icon-with-declaration.js",
-      };
-      config.module.rules[0].use[0].options.stripdeclarations = false;
+        entry: './test/input/icon-with-declaration.js'
+      }
+      config.module.rules[0].use[0].options.stripdeclarations = false
 
-      await runWebpack(config);
-      const encoded = await evaluateGeneratedBundle();
-      expect(encoded).toContain("%3c?xml version='1.0' encoding='UTF-8'?%3e");
-    });
+      const assetName = await runWebpack(config)
+      const encoded = await evaluateGeneratedBundle(assetName)
+      expect(encoded).toContain("%3c?xml version='1.0' encoding='UTF-8'?%3e")
+    })
 
     it("if turned on - should do nothing to an SVG that doesn't have an XML declaration", async () => {
       const config = {
         ...getBaseWebpackConfig(),
-        entry: "./test/input/icon.js",
-      };
-      config.module.rules[0].use[0].options.stripdeclarations = true;
+        entry: './test/input/icon.js'
+      }
+      config.module.rules[0].use[0].options.stripdeclarations = true
 
-      await runWebpack(config);
-      const encoded = await evaluateGeneratedBundle();
-      expect(encoded.startsWith("data:image/svg+xml,%3csvg")).toBe(true);
-    });
+      const assetName = await runWebpack(config)
+      const encoded = await evaluateGeneratedBundle(assetName)
+      expect(encoded.startsWith('data:image/svg+xml,%3csvg')).toBe(true)
+    })
 
-    it("if turned on - should remove XML declaration from a file that has one", async () => {
+    it('if turned on - should remove XML declaration from a file that has one', async () => {
       const config = {
         ...getBaseWebpackConfig(),
-        entry: "./test/input/icon-with-declaration.js",
-      };
-      config.module.rules[0].use[0].options.stripdeclarations = true;
+        entry: './test/input/icon-with-declaration.js'
+      }
+      config.module.rules[0].use[0].options.stripdeclarations = true
 
-      await runWebpack(config);
-      const encoded = await evaluateGeneratedBundle();
-      expect(encoded).not.toContain(
-        '%3c?xml version="1.0" encoding="UTF-8"?%3e'
-      );
-      expect(encoded.startsWith("data:image/svg+xml,%3csvg")).toBe(true);
-    });
-  });
+      const assetName = await runWebpack(config)
+      const encoded = await evaluateGeneratedBundle(assetName)
+      expect(encoded).not.toContain('%3c?xml version="1.0" encoding="UTF-8"?%3e')
+      expect(encoded.startsWith('data:image/svg+xml,%3csvg')).toBe(true)
+    })
+  })
 
   describe('"limit" option and "url.dataUrlLimit" configuration', function () {
     it('should fall back to file-loader if the content of SVG file is longer than "limit" query parameter', async () => {
       const config = {
         ...getBaseWebpackConfig(),
-        entry: "./test/input/icon.js",
-      };
-      config.module.rules[0].use[0].options.limit = 10;
-      config.module.rules[0].use[0].options.name = "foo.svg";
+        entry: './test/input/icon.js'
+      }
+      config.module.rules[0].use[0].options.limit = 10
+      config.module.rules[0].use[0].options.name = 'foo.svg'
 
-      await runWebpack(config);
-      const encoded = await evaluateGeneratedBundle();
+      const assetName = await runWebpack(config)
+      const encoded = await evaluateGeneratedBundle(assetName)
       expect(encoded).toEqual(
         expect.objectContaining({
-          default: "foo.svg",
+          default: 'foo.svg'
         })
-      );
-    });
-  });
+      )
+    })
+  })
 
-  describe("combining with other loaders", function () {
-    it("should convert SVG file to utf-8 encoded data-uri string, when embedded in LESS file", async () => {
+  describe('combining with other loaders', function () {
+    it('should convert SVG file to utf-8 encoded data-uri string, when embedded in LESS file', async () => {
       const config = {
         ...getBaseWebpackConfig(),
-        entry: "./test/input/less.js",
-      };
+        entry: './test/input/less.js'
+      }
       config.module.rules.push({
         test: /\.less$/,
-        use: ["css-loader", "less-loader"],
-      });
+        use: ['css-loader', 'less-loader']
+      })
 
-      await runWebpack(config);
-      const encoded = await evaluateGeneratedBundle();
+      const assetName = await runWebpack(config)
+      const encoded = await evaluateGeneratedBundle(assetName)
       expect(encoded.default.toString()).toContain(
         'background-image: url("data:image/svg+xml,%3csvg'
-      );
-    });
+      )
+    })
 
-    it("should convert SVG file to utf-8 encoded data-uri string, when embedded in SCSS file", async () => {
+    it('should convert SVG file to utf-8 encoded data-uri string, when embedded in SCSS file', async () => {
       const config = {
         ...getBaseWebpackConfig(),
-        entry: "./test/input/scss.js",
-      };
+        entry: './test/input/scss.js'
+      }
       config.module.rules.push({
         test: /\.scss$/,
-        use: ["css-loader", "sass-loader"],
-      });
+        use: ['css-loader', 'sass-loader']
+      })
 
-      await runWebpack(config);
-      const encoded = await evaluateGeneratedBundle();
+      const assetName = await runWebpack(config)
+      const encoded = await evaluateGeneratedBundle(assetName)
       expect(encoded.default.toString()).toContain(
         'background-image: url("data:image/svg+xml,%3csvg'
-      );
-    });
+      )
+    })
 
-    it("should convert SVG file to utf-8 encoded data-uri string, when embedded in CSS file", async () => {
+    it('should convert SVG file to utf-8 encoded data-uri string, when embedded in CSS file', async () => {
       const config = {
         ...getBaseWebpackConfig(),
-        entry: "./test/input/css.js",
-      };
+        entry: './test/input/css.js'
+      }
       config.module.rules.push({
         test: /\.css$/,
-        use: ["css-loader"],
-      });
+        use: ['css-loader']
+      })
 
-      await runWebpack(config);
-      const encoded = await evaluateGeneratedBundle();
+      const assetName = await runWebpack(config)
+      const encoded = await evaluateGeneratedBundle(assetName)
       expect(encoded.default.toString()).toContain(
         'background-image: url("data:image/svg+xml,%3csvg'
-      );
-    });
-  });
+      )
+    })
+  })
 
   describe('"iesafe" option skips styled files encoded to more than 4kB', function () {
-    it("should encode file below limit", async () => {
+    it('should encode file below limit', async () => {
       const config = {
         ...getBaseWebpackConfig(),
-        entry: "./test/input/4047B-encoded-styled.js",
-      };
-      config.module.rules[0].use[0].options.iesafe = true;
+        entry: './test/input/4047B-encoded-styled.js'
+      }
+      config.module.rules[0].use[0].options.iesafe = true
 
-      await runWebpack(config);
-      const encoded = await evaluateGeneratedBundle();
-      expect(encoded.startsWith("data:image/svg+xml,%3csvg")).toBe(true);
-      expect(encoded.length).toBeLessThan(4096);
-    });
+      const assetName = await runWebpack(config)
+      const encoded = await evaluateGeneratedBundle(assetName)
+      expect(encoded.startsWith('data:image/svg+xml,%3csvg')).toBe(true)
+      expect(encoded.length).toBeLessThan(4096)
+    })
 
-    it("should encode file above limit without style-element", async () => {
+    it('should encode file above limit without style-element', async () => {
       const config = {
         ...getBaseWebpackConfig(),
-        entry: "./test/input/4104B-encoded-unstyled.js",
-      };
-      config.module.rules[0].use[0].options.iesafe = true;
+        entry: './test/input/4104B-encoded-unstyled.js'
+      }
+      config.module.rules[0].use[0].options.iesafe = true
 
-      await runWebpack(config);
-      const encoded = await evaluateGeneratedBundle();
-      expect(encoded.startsWith("data:image/svg+xml,%3csvg")).toBe(true);
-      expect(encoded.length).toBeGreaterThan(4096);
-    });
+      const assetName = await runWebpack(config)
+      const encoded = await evaluateGeneratedBundle(assetName)
+      expect(encoded.startsWith('data:image/svg+xml,%3csvg')).toBe(true)
+      expect(encoded.length).toBeGreaterThan(4096)
+    })
 
-    it("should fall back on file above limit with style-element", async () => {
+    it('should fall back on file above limit with style-element', async () => {
       const config = {
         ...getBaseWebpackConfig(),
-        entry: "./test/input/4099B-encoded-styled.js",
-      };
-      config.module.rules[0].use[0].options.iesafe = true;
-      config.module.rules[0].use[0].options.name = "foo.svg";
+        entry: './test/input/4099B-encoded-styled.js'
+      }
+      config.module.rules[0].use[0].options.iesafe = true
+      config.module.rules[0].use[0].options.name = 'foo.svg'
 
-      await runWebpack(config);
-      const encoded = await evaluateGeneratedBundle();
+      const assetName = await runWebpack(config)
+      const encoded = await evaluateGeneratedBundle(assetName)
       expect(encoded).toEqual(
         expect.objectContaining({
-          default: "foo.svg",
+          default: 'foo.svg'
         })
-      );
-    });
+      )
+    })
 
-    it("should fall back on file above limit with style-element even when base64 encoding", async () => {
+    it('should fall back on file above limit with style-element even when base64 encoding', async () => {
       const config = {
         ...getBaseWebpackConfig(),
-        entry: "./test/input/4099B-encoded-styled.js",
-      };
-      config.module.rules[0].use[0].options.iesafe = true;
-      config.module.rules[0].use[0].options.name = "foo.svg";
-      config.module.rules[0].use[0].options.encoding = "base64";
+        entry: './test/input/4099B-encoded-styled.js'
+      }
+      config.module.rules[0].use[0].options.iesafe = true
+      config.module.rules[0].use[0].options.name = 'foo.svg'
+      config.module.rules[0].use[0].options.encoding = 'base64'
 
-      await runWebpack(config);
-      const encoded = await evaluateGeneratedBundle();
+      const assetName = await runWebpack(config)
+      const encoded = await evaluateGeneratedBundle(assetName)
       expect(encoded).toEqual(
         expect.objectContaining({
-          default: "foo.svg",
+          default: 'foo.svg'
         })
-      );
-    });
-  });
+      )
+    })
+  })
 
-  describe("encoding is base64", function () {
-    it("should convert SVG file to base64 encoded data-uri string", async () => {
+  describe('encoding is base64', function () {
+    it('should convert SVG file to base64 encoded data-uri string', async () => {
       const config = {
         ...getBaseWebpackConfig(),
-        entry: "./test/input/icon.js",
-      };
-      config.module.rules[0].use[0].options.encoding = "base64";
+        entry: './test/input/icon.js'
+      }
+      config.module.rules[0].use[0].options.encoding = 'base64'
 
-      await runWebpack(config);
-      const encoded = await evaluateGeneratedBundle();
-      expect(encoded).not.toContain('"');
-      expect(encoded.startsWith("data:image/svg+xml;base64")).toBe(true);
-    });
+      const assetName = await runWebpack(config)
+      const encoded = await evaluateGeneratedBundle(assetName)
+      expect(encoded).not.toContain('"')
+      expect(encoded.startsWith('data:image/svg+xml;base64')).toBe(true)
+    })
 
-    it("supports passing options using resource query", async () => {
+    it('supports passing options using resource query', async () => {
       const config = {
         ...getBaseWebpackConfig(),
-        entry: "./test/input/icon-resource-query.js",
-      };
+        entry: './test/input/icon-resource-query.js'
+      }
 
-      await runWebpack(config);
-      const encoded = await evaluateGeneratedBundle();
-      expect(encoded).not.toContain('"');
-      expect(encoded.startsWith("data:image/svg+xml;base64")).toBe(true);
-    });
-  });
-});
+      const assetName = await runWebpack(config)
+      const encoded = await evaluateGeneratedBundle(assetName)
+      expect(encoded).not.toContain('"')
+      expect(encoded.startsWith('data:image/svg+xml;base64')).toBe(true)
+    })
+  })
+})
